f9f1143200e56fb9b1cabbc51e823b3eda09fb14
Surya Kumar <kumarsurya45394@gmail.com>
2026-02-26 00:50:48 +0530

    debug

--- backend/configs/cors-config.js
+++ backend/configs/cors-config.js
@@ -1,11 +1,7 @@
 const cors=require('cors');
 const corsConfig=()=>{
     return cors({
-        origin: [
-            "http://127.0.0.1:5500",
-            "http://localhost:5500",
-            "https://internship-assignment-ten-gules.vercel.app/"
-        ],
+        origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"]
