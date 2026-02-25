69d0ec5044434908f430d7fa8ad45858eab04bdc
Surya Kumar <kumarsurya45394@gmail.com>
2026-02-26 00:54:50 +0530

    little change in controllers

--- backend/configs/cors-config.js
+++ backend/configs/cors-config.js
@@ -1,7 +1,11 @@
 const cors=require('cors');
 const corsConfig=()=>{
     return cors({
-        origin: "*",
+        origin: [
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+            "https://internship-assignment-ten-gules.vercel.app"
+        ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"]
